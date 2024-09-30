2ba48e2b5a66b81d3508c975ee5dbc0ce207997f
3milion <167380947+3milion@users.noreply.github.com>
2024-09-30 17:12:59 -0600

    monitoreo_red.sh

--- monitoreo_red.sh
+++ monitoreo_red.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
-#Verificar si el comando ifstat está instalado
+#Check if the ifstat command is installed
 if ! command -v ifstat &> /dev/null;
 then
     echo "Error: ifstat no está instalado. Instálalo para volver a intentar."
     exit 1
 fi
 
-#Definir la función para monitorear la red
+#Define the function to monitor the network
 function network_monitoring {
     local n=$1
     for ((i=1; i<=n; i++)); do
@@ -16,32 +16,34 @@ function network_monitoring {
     done
 }
 
-#Solicitar opción
+#Request option
 read -p "Elija una de las siguientes opciones [1]-Ver el monitoreo de red en vivo \
 [2]-Ver el monitoreo de red cierta cantidad de veces [3]-Generar el monitoreo de red cierta cantidad de veces: " op
 
-#Validar la opción elegida por el usuario
-while ! [[ $op =~ ^[1-3]$ ]]; 
+#Validate the option chosen by the user
+while [ $op -ne 1 ] && [ $op -ne 2 ] && [ $op -ne 3 ]; 
 do
     read -p "Ingrese una opción correcta (1, 2 o 3): " op
 done
-#Se ejecuta la opcion deseada
+
+#The desired option is executed
 if [ $op -eq 1 ]; 
 then
-    # Ver el monitoreo indefinidamente
+    #View monitoring indefinitely
     echo "Presione Ctrl+C para salir"
     ifstat
 elif [ $op -eq 2 ];
 then
-    #Ver una cantidad definida de veces el monitoreo
+    #View a defined number of monitoring times
     read -p "Ingrese la cantidad de veces que quiere ver el monitoreo: " num
     network_monitoring $num
 else 
-    #Pedir el nombre del archivo y la cantidad de veces
+    #Prompt for file name and number of times
     read -p "Ingrese el nombre del reporte que quiere generar: " file
     read -p "Ingrese la cantidad de veces que quiere ver el monitoreo: " num
-    #redirigir la salida a un archivo
-    if ! network_monitoring $num > "$file"; then
+    #redirect output to a file
+    if ! network_monitoring $num > "$file"; 
+    then
         echo "Error: No se pudo generar el archivo"
         exit 1
     else
